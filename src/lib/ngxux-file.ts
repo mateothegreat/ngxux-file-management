295182c9587ef52354ef2ae20837b72ca27acada
Matthew Davis <matthew@matthewdavis.io>
2019-04-03 06:23:43 -0500

    bump

---
+++ src/lib/ngxux-file.ts
@@ -0,0 +1,13 @@
+export class NgxuxFile {
+
+    public id: number;
+    public uuid: string;
+    public stampCreated: string;
+    public downloadUrl: string;
+    public thumbnailUrl: string;
+    public type: string;
+    public bytes: number;
+    public belongsToType: string;
+    public belongsToUuid: string;
+    
+}
